38b1cefb0e972c79c00c6de62f5b80ea36d3b6ad
Laicasaane <laicasaane@outlook.com>
2026-07-03 15:36:46 +0700

    Samples: update

--- Packages/com.laicasaane.encosy-tower/Samples~/EncosyTower.Samples.Stats/EncosyTower.Samples.Stats.Systems/StatModifiersRemoveSystem.cs
+++ Packages/com.laicasaane.encosy-tower/Samples~/EncosyTower.Samples.Stats/EncosyTower.Samples.Stats.Systems/StatModifiersRemoveSystem.cs
@@ -1,10 +1,7 @@
 using Unity.Burst;
-using Unity.Burst.Intrinsics;
 using Unity.Collections;
-using Unity.Collections.LowLevel.Unsafe;
 using Unity.Entities;
 using Unity.Jobs;
-using UnityEngine.Scripting;
 
 namespace EncosyTower.Samples.Stats
 {
