b5b515c9713f70401bd7ac9c0e6a2a58fdd2bd17
Laicasaane <laicasaane@outlook.com>
2026-07-03 18:41:04 +0700

    SourceGen: rebuild for 0.1.6-preview.7

--- Plugins/SourceGenerator/EncosyTower.SourceGen.Helpers/Helpers/SourceGenVersion.cs
+++ Plugins/SourceGenerator/EncosyTower.SourceGen.Helpers/Helpers/SourceGenVersion.cs
@@ -2,6 +2,6 @@ namespace EncosyTower.SourceGen
 {
     public static class SourceGenVersion
     {
-        public const string VALUE = "0.1.6-preview.6";
+        public const string VALUE = "0.1.6-preview.7";
     }
 }
